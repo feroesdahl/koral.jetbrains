6a096520ab6267e5e6586f6e217cf6d4a913b262
Frederik Erslev Roesdahl <fe@roesdahl.com>
2026-08-24 20:52:42 +0200

    update plugin.xml

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,7 +5,6 @@ rootProject.name = "IntelliJ Platform Plugin Template"
 pluginManagement {
     plugins {
         id("org.jetbrains.kotlin.jvm") version "2.1.20"
-        id("org.jetbrains.changelog") version "2.5.0"
     }
 }
 
